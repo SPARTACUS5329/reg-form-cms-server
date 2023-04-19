781665a9249c694dfc0ee7c2d20b8eb9f7f64f8a
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2023-04-19 20:19:58 +0530

    added update form

--- src/controllers/FormController.ts
+++ src/controllers/FormController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Form } from "../models/form";
+import { Form, FormDocument } from "../models/form";
 
 export const foo = (req: Request, res: Response) => {
 	res.status(200).send("It is working dumbass");
@@ -20,6 +20,19 @@ export const createForm = async (req: Request, res: Response) => {
 	try {
 		const form = new Form(req.body);
 		await form.save();
+		return res.status(200).send("SUCCESS");
+	} catch (error: any) {
+		console.error(error);
+		return res.status(500).send(error.message);
+	}
+};
+
+export const updateForm = async (req: Request, res: Response) => {
+	try {
+		const { slug: name } = req.params;
+		const { newForm }: { newForm: FormDocument } = req.body;
+		console.log(newForm);
+		await Form.findOneAndUpdate({ name }, newForm);
 		return res.status(200).send("SUCCESS");
 	} catch (error: any) {
 		console.error(error);
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { createForm, foo, getForms } from "../controllers/FormController";
+import { createForm, foo, getForms, updateForm } from "../controllers/FormController";
 import { getRegistrations, register } from "../controllers/RegistrationController";
 
 const router: Router = express.Router();
@@ -7,6 +7,7 @@ const router: Router = express.Router();
 router.get("/", foo);
 router.post("/forms", getForms);
 router.post("/create-form", createForm);
+router.patch("/update-form/:slug", updateForm);
 
 router.get("/register/:slug", getRegistrations);
 router.post("/register/:slug", register);
